6019559229f5ce712ee9c9511e66f81d70031c47
Jamie Gilkeson <rangedenemy@gmail.com>
2016-03-02 11:08:37 -0500

    Added basic framework and inital main layout as well as minor content for home page

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pjjam
 A node web app to display board game win statistics
+
+#Set up.
+This app will run on port 3002 and is an express and node based web app
+Currently to properly read data a script must be used to regularly download a .csv file from your google sheet produced by the google form for collecting game data.
---
+++ pjjam.js
@@ -0,0 +1,47 @@
+//require express and set up express/handlebars for main workload
+var express = require('express');
+var app=express();
+//set deafult layout to main and add support for sections
+var handlebars = require('express3-handlebars').create({ defaultLayout:'main' , helpers: {
+    section: function(name, options){
+	if(!this._sections) this._sections={};
+	this._sections[name]=options.fn(this);
+	return null;
+    }
+}});
+//require d3 for data visualization
+var d3=require('d3');
+app.engine('handlebars', handlebars.engine);
+//set public to usable
+app.use(express.static(__dirname + '/public'));
+//allow for tests to be show
+app.use(function(req, res, next){
+    res.locals.showTests=app.get('env') !== 'production' && req.query.test=='1';
+    next();
+});
+//set view engine to handlebars
+app.set('view engine', 'handlebars');
+//set port to 3002
+app.set('port', process.env.PORT || 3002);
+
+
+app.get('/', function(req, res){
+    res.locals.home=true;
+    res.render('home');
+});
+
+app.use(function(req, res){
+    res.status(404);
+    res.render('404', {layout: false});
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500', {layout: false});
+});
+
+app.listen(app.get('port'), function(){
+ console.log( 'Express started on http://localhost:' +
+ app.get('port') + '; press Ctrl-C to terminate.' );
+});
